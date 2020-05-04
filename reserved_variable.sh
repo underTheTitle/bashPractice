7e5872d711eae83716e68f10e2ad7ba6713964a8
underthetitle <underthetitle@gmail.com>
2020-05-04 19:15:54 +0900

    예약 변수 정리

--- reserved_variable.sh
+++ reserved_variable.sh
@@ -1,4 +1,85 @@
 # 예약 변수
-echo "HOME: "
+echo "HOME: 사용자의 홈 디렉토리"
 echo "HOME: "${HOME}
+echo "PATH: 실행파일을 찾을 경로"
 echo "PATH: "${PATH}
+echo "LANG: 프로그램 사용 시 기본 지원되는 언어"
+echo "LANG: "${LANG}
+echo "PWD: 사용자의 현재 작업중인 디렉토리"
+echo "PWD: "${PWD}
+echo "FUNCNAME: 현재 함수 이름"
+echo "FUNCNAME: "${FUNCNAME}
+echo "SECONDS: 스크립트가 실행된 초 단위 시간"
+echo "SECONDS: "${SECONDS}
+echo "SHLVL: 쉘 레벨(중첩된 깊이를 나타냄)"
+echo "SHLVL: "${SHLVL}
+echo "SHELL: 로그인해서 사용하는 쉘"
+echo "SHELL: "${SHELL}
+echo "PPID: 부모 프로세스의 PID"
+echo "PPID: "${PPID}
+echo "BASH: BASH 실행파일 경로"
+echo "BASH: "${BASH}
+echo "BASH_ENV: 스크립트 실행시 BASH 시작 파일을 읽을 위치 변수"
+echo "BASH_ENV: "${BASH_ENV}
+echo "BASH_VERSION: 설치된 BASH 버전"
+echo "BASH_VERSION: "${BASH_VERSION}
+echo "BASH_VERSINFO: BASH_VERSINFO[0] ~ BASH_VERSINFO[5] 배열로 상세정보 제공"
+echo "BASH_VERSINFO: "${BASH_VERSINFO}
+echo "MAIL: MAIL 보관 경로"
+echo "MAIL: "${MAIL}
+echo "MAILCHECK: 메일 확인 시간"
+echo "MAILCHECK: "${MAILCHECK}
+echo "OSTYPE: 운영체제 종류"
+echo "OSTYPE: "${OSTYPE}
+echo "TERM: 로그인 터미널 타입"
+echo "TERM: "${TERM}
+echo "HOSTNAME: 호스트 이름"
+echo "HOSTNAME: "${HOSTNAME}
+echo "HOSTTYPE: 시스템 하드웨어 종류"
+echo "HOSTTYPE: "${HOSTTYPE}
+echo "MACHTYPE: 머신 종류(HOSTTYPE과 같은 정보지만 조금 더 상세하게 표시됨"
+echo "MACHTYPE: "${MACHTYPE}
+echo "LOGNAME: 로그인 이름"
+echo "LOGNAME: "${LOGNAME}
+echo "UID: 사용자 UID"
+echo "UID: "${UID}
+echo "EUID: su 명령에서 사용하는 사용자의 유효 아이디값(UID와 EUID값은 다를 수 있음)"
+echo "EUID: "${EUID}
+echo "USER: 사용자의 이름"
+echo "USER: "${USER}
+echo "USERNAME: 사용자 이름"
+echo "USERNAME: "${USERNAME}
+echo "GROUPS: 사용자 그룹(/etc/passwd 값을 출력)"
+echo "GROUPS: "${GROUPS}
+echo "HISTFILE: history 파일 경로"
+echo "HISTFILE: "${HISTFILE}
+echo "HISTFILESIZE: history 파일 크기"
+echo "HISTFILESIZE: "${HISTFILESIZE}
+echo "HISTSIZE: history 저장되는 개수"
+echo "HISTSIZE: "${HISTSIZE}
+echo "HISTCONTROL: 중복되는 명령에 대한 기록 유무"
+echo "HISTCONTROL: "${HISTCONTROL}
+echo "DISPLAY: x디스플레이 이름"
+echo "DISPLAY: "${DISPLAY}
+echo "IFS: 입력 필드 구분자(기본값: 빈칸)"
+echo "IFS: "${IFS}
+echo "VISUAL: VISUAL 편집기 이름"
+echo "VISUAL: "${VISUAL}
+echo "EDITOR: 기본 편집기 이름"
+echo "EDITOR: "${EDITOR}
+echo "COLUMNS: 현재 터미널이나 윈도우 터미널의 컬럼 수"
+echo "COLUMNS: "${COLUMNS}
+echo "LINES: 터미널의 라인 수"
+echo "LINES: "${LINES}
+echo "LS_COLORS: ls 명령의 색상 관련 옵션"
+echo "LS_COLORS: "${LS_COLORS}
+echo "PS1: 기본 프롬프트 변수(기본값: bash\$)"
+echo "PS1: "${PS1}
+echo "PS2: 보조 프롬프트 변수(기본값: >), 명령을 \"\\\"를 사용하여 명령행을 연장시 사용됨"
+echo "PS2: "${PS2}
+echo "PS3: 쉘 스크립트에서 select 사용시 프롬프트 변수(기본값: #?)"
+echo "PS3: "${PS3}
+echo "PS4: 쉘 스크립트 디버깅 모드의 프롬프트 변수(기본값: +)"
+echo "PS4: "${PS4}
+echo "TMOUT: 0이면 제한이 없으며 time 시간 지정시 지정한 시간 이후 로그아웃"
+echo "TMOUT: "${TMOUT}
